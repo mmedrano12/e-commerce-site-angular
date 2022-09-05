ccb3f7b9e666c66e3f0f89174f70e349527ef9ea
Matthew Medrano <medran16@students.rowan.edu>
2022-09-05 17:40:08 -0400

    Completed project

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -10,11 +12,26 @@ export class CartComponent implements OnInit {
   //getItems call from cartService to retrieve the list of items in the cart
   items = this.cartService.getItems();
 
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  })
+
   constructor(
     private cartService: CartService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+  }
+
+  //lets users submit their name and address, also the cart clears itself after the order is sent
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your Order has been submitted', this.checkoutForm.value);
+    window.alert('Thank you for your purchase!')
+    this.checkoutForm.reset();
   }
 
 }
